0a16c2509c64d62bff0f5399803eb308f60d232b
edman <edman@LAPTOP-IUQHGQ9D>
2021-03-24 00:05:03 -0600

    stats query implemented

--- server.js
+++ server.js
@@ -46,8 +46,53 @@ app.get('/users/login', checkAuthenticated, (req, res) => {
 });
 
 app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
-    res.render('dashboard', {user: req.user.nombre, email: req.user.correo,
-         subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario});
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query (
+                `SELECT nombre_playlist from playlists
+                WHERE EXISTS( SELECT nombre_playlist from playlists WHERE id = $1)`, [req.user.id] );
+
+            let recentAlbum = await client.query (`SELECT * FROM albums WHERE fecha_album = (
+                SELECT MAX(fecha_album) FROM albums)`);
+
+            let popularArtist = await client.query(`SELECT a.nombre, COUNT(*) FROM playlists c 
+                INNER JOIN artistas a ON a.codigo_artista = c.codigo_artista
+                INNER JOIN albums b ON c.codigo_album = b.codigo_album GROUP BY a.nombre order by 2 desc`);
+
+            let newSubscriptions = await client.query(`SELECT COUNT(*) FROM suscripciones
+                WHERE fecha_de_suscripcion > current_date - interval '3 month'`);
+
+            let withMoreProduction = await client.query(`SELECT a.nombre, COUNT(*) FROM canciones c 
+                INNER JOIN artistas a ON a.codigo_artista=c.codigo_artista GROUP BY a.nombre ORDER BY 2 DESC LIMIT 3`);
+            
+            let popularGenre = await client.query(`SELECT g.descripcion_genero, COUNT(*) FROM canciones c 
+            INNER JOIN genero g ON g.codigo_genero=c.codigo_genero GROUP BY g.descripcion_genero ORDER BY 2 DESC`);
+
+            let amountLog = await client.query(`SELECT correo, cantidad_veces_logeado FROM usuarios 
+                ORDER BY 2 desc`)
+   
+                res.render('dashboard', {user: req.user.nombre, email: req.user.correo,
+                    subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario,
+                    amountPlays: req.user.cantidad_canciones_usuarios, 
+                    playlists: resp.rows.length ? resp.rows[0]["nombre_playlist"] : '',
+                    mostRecentAlbum: recentAlbum.rows[0]["nombre_album"],
+                    mostPopularArtist: popularArtist.rows[0]["nombre"],
+                    mostPopularArtist2: popularArtist.rows[1]["nombre"],
+                    mostPopularArtist3: popularArtist.rows[2]["nombre"],
+                    numberNewSubscriptions: newSubscriptions.rows[0]["count"],
+                    artistWithMoreProduction: withMoreProduction.rows[0]["nombre"],
+                    artistWithMoreProduction2: withMoreProduction.rows[1]["nombre"],
+                    artistWithMoreProduction3: withMoreProduction.rows[2]["nombre"],
+                    popularGenres: popularGenre.rows[0]["descripcion_genero"],
+                    popularGenres2: popularGenre.rows[1]["descripcion_genero"],
+                    popularGenres3: popularGenre.rows[2]["descripcion_genero"],
+                    amountLogs: amountLog.rows[0]["correo"],
+                    amountLogs2: amountLog.rows[1]["correo"],
+                    amountLogs3: amountLog.rows[2]["correo"]});
+        })
+    } catch(error) {
+        console.log(error);
+    }
 });
 
 app.get('/users/playlist', (req, res) => {
@@ -62,27 +107,34 @@ app.get('/users/actualizar', (req, res) => {
     res.render("actualizar");
 });
 
+app.get('/users/canciones', (req, res) => {
+    res.render("canciones");
+});
+
 app.get('/users/artista', (req, res) => {
 
-    async function getName() {
-        
-        const result = await pool.query(
-            `SELECT nombre from artistas WHERE codigo_artista = $1`, 
-            ['7']
-        );
-        if (!result || !result.rows || !result.rows.length) return null;
-        // return result.rows[0]["nombre"];
-        res.render('artista', {user: req.user.nombre, email: req.user.correo,
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query (`SELECT nombre FROM artistas 
+            WHERE EXISTS(SELECT nombre FROM artistas WHERE codigo_artista = $1)`, [req.user.id.toString()]);
+
+            res.render('artista', {user: req.user.nombre, email: req.user.correo,
             subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario, 
-            artistName: result.rows[0]["nombre"]});
+            artistName: resp.rows[0]["nombre"]});
+
+        })
+    } catch(error) {
+        console.log(error);
     }
-
-    getName();
-
 });
 
 app.get('/users/agregarnombreartista', (req, res) => {
     res.render('agregarnombreartista', {user: req.user.nombre, email: req.user.correo,
+        subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario});
+});
+
+app.get('/users/addtrack', (req, res) => {
+    res.render('addtrack', {user: req.user.nombre, email: req.user.correo,
         subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario});
 });
 
@@ -91,13 +143,9 @@ app.get("/users/logout", (req, res) => {
     res.render("index", { message: "You have logged out successfully" });
 });
 
-app.post('/users/dashboard', async(req, res) => {
-    let { playlist } = req.body;
-    pool.query(
-        `INSERT INTO playlists (id, codigo_cancion, codigo_artista, codigo_album, nombre_playlist)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING id`,
-        [req.user.id, '1', '1', '1', playlist],
+app.post('/users/dashboard', async (req, res) => {
+    pool.query(`UPDATE usuarios SET cantidad_canciones_usuarios = $1 WHERE id = $2`, 
+        [2, 7],
         (err, result) => {
             if (err){
                 throw err;
@@ -147,24 +195,40 @@ app.post('/users/premium', async(req, res) => {
             if (err){
                 throw err;
             }
-            console.log(`${req.user.correo}`);
             res.redirect('/users/dashboard');
         }  
     )
 });
 
 app.post('/users/actualizar', async(req, res) => {
-    
-    pool.query(
-        `UPDATE usuarios SET codigo_tipo_usuario = $1 WHERE correo = $2`,
-        [1, req.user.correo],
-        (err, result) => {
-            if (err){
-                throw err;
-            }
+
+    try {
+        pool.connect(async (error, client, release) => {
+            await client.query (`UPDATE usuarios SET 
+            codigo_tipo_usuario = $1 WHERE correo = $2`, [1, req.user.correo]);
+        
+            await client.query(`INSERT INTO artistas 
+            (codigo_artista, nombre) VALUES ($1, $2)`, [req.user.id.toString(), req.user.nombre]);
+       
+            res.redirect('/users/artista');
+        })
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+app.post('/users/canciones', async(req, res) => {
+
+    try {
+        pool.connect(async (error, client, release) => {
+            await client.query (`UPDATE usuarios SET cantidad_canciones_usuarios = $1 WHERE id = $2`,
+            [parseInt(req.user.cantidad_canciones_usuarios) + 1, req.user.id]);
+       
             res.redirect('/users/dashboard');
-        }  
-    )
+        })
+    } catch(error) {
+        console.log(error);
+    }
 });
 
 app.post('/users/register', async (req, res) => {
@@ -235,7 +299,7 @@ function checkAuthenticated(req, res, next) {
     next();
 }
   
-  function checkNotAuthenticated(req, res, next) {
+function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
     }
