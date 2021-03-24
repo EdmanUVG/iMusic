6b919976129eb98071bc3553818aa1c5ef6b265c
edman <edman@LAPTOP-IUQHGQ9D>
2021-03-24 17:31:01 -0600

    usuarios agregados

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
         pool.connect(async (error, client, release) => {
             let resp = await client.query (
                 `SELECT nombre_playlist from playlists
-                WHERE EXISTS( SELECT nombre_playlist from playlists WHERE id = $1)`, [req.user.id] );
+                WHERE EXISTS( SELECT nombre_playlist FROM playlists WHERE id = $1 LIMIT 5)`, [req.user.id] );
 
             let recentAlbum = await client.query (`SELECT * FROM albums WHERE fecha_album = (
                 SELECT MAX(fecha_album) FROM albums)`);
@@ -69,12 +69,12 @@ app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
             INNER JOIN genero g ON g.codigo_genero=c.codigo_genero GROUP BY g.descripcion_genero ORDER BY 2 DESC`);
 
             let amountLog = await client.query(`SELECT correo, cantidad_veces_logeado FROM usuarios 
-                ORDER BY 2 desc`)
+                ORDER BY 2 desc`);
    
-                res.render('dashboard', {user: req.user.nombre, email: req.user.correo,
+            res.render('dashboard', {user: req.user.nombre, email: req.user.correo,
                     subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario,
                     amountPlays: req.user.cantidad_canciones_usuarios, 
-                    playlists: resp.rows.length ? resp.rows[0]["nombre_playlist"] : '',
+                    playlists: resp.rows,
                     mostRecentAlbum: recentAlbum.rows[0]["nombre_album"],
                     mostPopularArtist: popularArtist.rows[0]["nombre"],
                     mostPopularArtist2: popularArtist.rows[1]["nombre"],
@@ -111,6 +111,22 @@ app.get('/users/canciones', (req, res) => {
     res.render("canciones");
 });
 
+app.get('/users/deleteArtist', (req, res) => {
+    res.render("deleteArtist");
+});
+
+app.get('/users/updateartist', (req, res) => {
+    res.render("updateartist");
+});
+
+app.get('/users/udpatealbum', (req, res) => {
+    res.render("udpatealbum");
+});
+
+app.get('/users/updatetrack', (req, res) => {
+    res.render("updatetrack");
+});
+
 app.get('/users/artista', (req, res) => {
 
     try {
@@ -121,7 +137,6 @@ app.get('/users/artista', (req, res) => {
             res.render('artista', {user: req.user.nombre, email: req.user.correo,
             subscription: req.user.codigo_suscripcion, userRole: req.user.codigo_tipo_usuario, 
             artistName: resp.rows[0]["nombre"]});
-
         })
     } catch(error) {
         console.log(error);
@@ -174,9 +189,8 @@ app.post('/users/playlist', async(req, res) => {
 app.post('/users/agregarnombreartista', async(req, res) => {
     let { nombre } = req.body;
     pool.query(
-        `INSERT INTO artistas (codigo_artista, nombre)
-        VALUES ($1, $2)`,
-        [req.user.id, nombre],
+        `UPDATE artistas SET nombre = $1 WHERE codigo_artista = $2;`,
+        [nombre, req.user.id.toString()],
         (err, result) => {
             if (err){
                 throw err;
@@ -208,9 +222,101 @@ app.post('/users/actualizar', async(req, res) => {
             codigo_tipo_usuario = $1 WHERE correo = $2`, [1, req.user.correo]);
         
             await client.query(`INSERT INTO artistas 
-            (codigo_artista, nombre) VALUES ($1, $2)`, [req.user.id.toString(), req.user.nombre]);
+            (codigo_artista, nombre) VALUES ($1, $2)`, [req.user.id.toString(), req.user.nombre],
+                (err, result) => {
+                    if (err){
+                        throw err;
+                    }
+                    res.redirect('/users/artista');
+                }   
+            );
        
-            res.redirect('/users/artista');
+            
+        })
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+app.post('/users/deleteArtist', async(req, res) => {
+
+    let { nombre } = req.body;
+
+    try {
+        pool.connect(async (error, client, release) => {
+        
+            await client.query(`DELETE FROM artistas WHERE nombre = $1`, [nombre],
+                (err) => {
+                    if (err){
+                        throw err;
+                    }
+                    req.flash("success_msg", "¡Artista fue eliminado exitosamente!");
+                    res.redirect('/users/dashboard');
+                }   
+            );    
+        })
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+app.post('/users/updateartist', async(req, res) => {
+
+    let { nombre, nuevo } = req.body;
+
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query(`UPDATE artistas set nombre = $1 WHERE nombre = $2`, [nuevo, nombre],
+                (err) => {
+                    if (err){
+                        throw err;
+                    }
+                    req.flash("success_msg", "¡Artista fue actualizada exitosamente!");
+                    res.redirect('/users/dashboard');
+                }   
+            );            
+        })
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+app.post('/users/udpatealbum', async(req, res) => {
+
+    let { nombre, nuevo } = req.body;
+
+    try {
+        pool.connect(async (error, client, release) => {
+            await client.query(`UPDATE albums SET nombre_album = $1 WHERE nombre_album = $2`, [nuevo, nombre],
+                (err) => {
+                    if (err){
+                        throw err;
+                    }
+                    req.flash("success_msg", "¡Álbum fue actualizada exitosamente!");
+                    res.redirect('/users/dashboard');
+                }   
+            );
+        })
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+app.post('/users/updatetrack', async(req, res) => {
+
+    let { nombre, nuevo } = req.body;
+
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query(`UPDATE canciones SET nombre_cancion = $1 WHERE nombre_cancion = $2`, [nuevo, nombre]);
+  
+            if (resp === "" || resp === null) {
+                req.flash("error_msg", "¡La canción no esta en la base de datos!");
+                res.redirect('/users/dashboard');
+            } else {
+                req.flash("success_msg", "¡La canción fue actualizada exitosamente!");
+                res.redirect('/users/dashboard');
+            }
         })
     } catch(error) {
         console.log(error);
@@ -265,15 +371,16 @@ app.post('/users/register', async (req, res) => {
                    res.render("register", { errors });
                } else {
                    pool.query(
-                        `INSERT INTO usuarios (correo, codigo_suscripcion, codigo_tipo_usuario, contrasena, nombre)
-                        VALUES ($1, $2, $3, $4, $5)
+                        `INSERT INTO usuarios (correo, codigo_suscripcion, codigo_tipo_usuario, contrasena, nombre,
+                            cantidad_veces_logeado, cantidad_canciones_usuarios)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7)
                         RETURNING id, contrasena`,
-                        [email, 0, 0, hashedPassword, name],
+                        [email, 0, 0, hashedPassword, name, 1, 0],
                         (err, result) => {
                             if (err){
                                 throw err;
                             }
-                            req.flash("success_msg", "You are now registered. Please log in");
+                            req.flash("success_msg", "La cuenta fue creada exitosamente. Ahora ingrese abajo");
                             res.redirect('/users/login');
                         }   
                     );
@@ -283,17 +390,26 @@ app.post('/users/register', async (req, res) => {
     }
 });
 
-app.post(
-    "/users/login",
-    passport.authenticate("local", {
+app.post("/users/login", passport.authenticate("local", {
       successRedirect: "/users/dashboard",
       failureRedirect: "/users/login",
-      failureFlash: true
-    })
+      failureFlash: true,
+    } ) 
+
+    
 );
 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
+        try {
+            pool.connect(async (error, client, release) => {
+    
+                await client.query (`UPDATE usuarios SET 
+                cantidad_veces_logeado = $1 WHERE correo = $2`, [parseInt(req.user.cantidad_veces_logeado) + 1, req.user.correo]);
+            })
+        } catch(error) {
+            console.log(error);
+        }  
       return res.redirect("/users/dashboard");
     }
     next();
